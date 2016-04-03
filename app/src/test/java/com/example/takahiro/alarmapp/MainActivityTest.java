2e3732211100ac20893b3f514e64c9783a0ff812
takaneo <gitroid@yahoo.co.jp>
2016-04-03 11:25:28 +0900

    testCommit

---
+++ app/src/test/java/com/example/takahiro/alarmapp/MainActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.takahiro.alarmapp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Takahiro on 2016/03/26.
+ */
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+
+    }
+}
